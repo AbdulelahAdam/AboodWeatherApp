4feae406f3d37df7cc29e96a7779e7658a222743
Abdulelah Adam <abdulelah859@gmail.com>
2020-09-12 18:25:46 +0200

    Resolved an issue with the Date

--- app.js
+++ app.js
@@ -54,7 +54,7 @@ function getWeather() {
 
 
 function getDate() {
-    const dayName = ["Saturday", "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday"];
+    const dayName = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
     const monthName = ["January", "February", "March", "April", "May", "June",
         "July", "August", "September", "October", "November", "December"
     ];
